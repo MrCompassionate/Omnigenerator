6179c883a035ad04fec66b40bd704dbc17cee0ee
MrCompassionate <marcuswatkissveal@gmail.com>
2023-08-02 14:55:33 +0100

    working hex grid

--- src/DiagramsTutorial.hs
+++ src/DiagramsTutorial.hs
@@ -9,12 +9,14 @@ horizontal 0 = node 6
 horizontal x = node 6 ||| horizontal (x-1) 
 
 vertical :: Int -> Diagram B
-vertical 0 = node 6
-vertical y = horizontal 8 <> vertical (y-1) # translate (r2 (0.86, 1.54))
-vertical y = horizontal 8 <> vertical (y-1) # translate (r2 (0.86, 1.54))   
+vertical x
+    | x <= 0 = node 0
+    | even x = horizontal 8 <> vertical (x-1) # translate (r2 (-0.86, 1.54))
+    | odd x = horizontal 8 <> vertical (x-1) # translate (r2 (0.86, 1.54))   
+    | otherwise = node 6
 
 -- vertical y = horizontal 8 === vertical (y-1) # translate (r2 (0.9, -1))   
 -- snugY 0.1 (regPoly x 1) # rotateBy (0.5/6)
-example = vertical 4
+example = vertical 8
 
 main = mainWith example 
