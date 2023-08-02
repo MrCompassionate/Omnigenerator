1e10c29f23cfc71224a50b9b77ec8bbf0ce09303
MrCompassionate <marcuswatkissveal@gmail.com>
2023-08-02 13:44:53 +0100

    Cabal keeps not working

--- src/DiagramsTutorial.hs
+++ src/DiagramsTutorial.hs
@@ -10,8 +10,10 @@ horizontal x = node 6 ||| horizontal (x-1)
 
 vertical :: Int -> Diagram B
 vertical 0 = node 6
-vertical y = horizontal 8 === vertical (y-1) # translate (r2 (0.9, -1))   
+vertical y = horizontal 8 === vertical (y-1) 
 
+-- vertical y = horizontal 8 === vertical (y-1) # translate (r2 (0.9, -1))   
+-- snugY 0.1 (regPoly x 1) # rotateBy (0.5/6)
 example = vertical 4
 
 main = mainWith example 
