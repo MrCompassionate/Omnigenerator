e8d42ea852ee21c9f38c8605e362bd00cc37ddc2
MrCompassionate <marcuswatkissveal@gmail.com>
2023-08-02 14:21:04 +0100

    hahahaha

--- src/DiagramsTutorial.hs
+++ src/DiagramsTutorial.hs
@@ -10,7 +10,8 @@ horizontal x = node 6 ||| horizontal (x-1)
 
 vertical :: Int -> Diagram B
 vertical 0 = node 6
-vertical y = horizontal 8 === vertical (y-1) 
+vertical y = horizontal 8 <> vertical (y-1) # translate (r2 (0.86, 1.54))
+vertical y = horizontal 8 <> vertical (y-1) # translate (r2 (0.86, 1.54))   
 
 -- vertical y = horizontal 8 === vertical (y-1) # translate (r2 (0.9, -1))   
 -- snugY 0.1 (regPoly x 1) # rotateBy (0.5/6)
